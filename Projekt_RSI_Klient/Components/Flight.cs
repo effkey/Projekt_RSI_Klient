915bda941d0dd99a1de0d4cfdefaddf866d54c7f
Jakub Pietyszuk <130553257+kubpi@users.noreply.github.com>
2024-05-11 22:09:19 +0200

    Dodaj pliki projektów.

---
+++ Projekt_RSI_Klient/Components/Flight.cs
@@ -0,0 +1,12 @@
+﻿namespace Projekt_RSI_Klient.Components
+{
+    public class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string CityFrom { get; set; }
+        public string CityTo { get; set; }
+        public string Day { get; set; }
+        public string Time { get; set; }
+    }
+
+}
